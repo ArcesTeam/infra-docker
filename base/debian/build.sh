d7af85583f0745e4d1eb9bd4fb9debf8bfa0ceb9
John <Tierney-john@hotmail.com>
2025-06-02 12:05:52 +0800

    [Dev] ref #3 : 构建通用镜像配置-补充debian镜像

---
+++ base/debian/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2025 ArcesTeam. All rights reserved.
+# This project is licensed under the MIT License. See the LICENSE file for details.
+# This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+#
+#
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+read_version() {
+    if [[ ! -f "VERSION" ]]; then
+        echo "VERSION file not found. Please create a VERSION file with the image version."
+        exit 1
+    fi
+
+    local version
+    version=$(< VERSION)
+    version="${version//[$'\t\r\n ']}"  # Trim whitespace
+
+    if [ -z "$version" ]; then
+        echo "VERSION file is empty. Please provide a valid version."
+        exit 1
+    fi
+
+    echo "$version"
+}
+
+build() {
+    local image_name="debian"
+    local version
+    local tag
+
+    version=$(read_version)
+    tag="ghcr.io/arcesteam/infra-docker/base/$image_name:$version"
+
+    echo "Building Docker image: $image_name:$version"
+    docker build -t "$tag" \
+        --build-arg "IMAGE_NAME=$image_name" \
+        --build-arg "VERSION=$version" \
+        --file "Dockerfile" \
+        --progress=plain \
+        .
+    echo "Docker image built successfully: $image_name:$version"
+
+    echo "Pushing Docker image: $image_name:$version"
+    docker push "$tag"
+    echo "Docker image pushed successfully: $image_name:$version"
+}
+
+build
