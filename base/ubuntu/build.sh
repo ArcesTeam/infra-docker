30e2e992e22bea1d80a496328f7888b465b0e201
John <Tierney-john@hotmail.com>
2025-06-02 11:30:11 +0800

    [Dev] ref #3 : 构建通用镜像配置-Base层镜像构建

---
+++ base/ubuntu/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2025 ArcesTeam. All rights reserved.
+# This project is licensed under the MIT License. See the LICENSE file for details.
+# This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+#
+#
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+build() {
+    local image_name="ubuntu"
+    local version
+    local tag
+
+    if [[ ! -f "VERSION" ]]; then
+        echo "VERSION file not found. Please create a VERSION file with the image version."
+        exit 1
+    fi
+
+    version=$(cat "VERSION")
+    tag="ghcr.io/arcesteam/infra/base/$image_name:$version"
+
+    echo "Building Docker image: $image_name:$version"
+    docker build -t "$tag" \
+        --build-arg "IMAGE_NAME=$image_name" \
+        --build-arg "VERSION=$version" \
+        --file "Dockerfile" \
+        --progress=plain \
+        .
+    echo "Docker image built successfully: $image_name:$version"
+
+    echo "Pushing Docker image: $image_name:$version"
+    docker push "$tag"
+    echo "Docker image pushed successfully: $image_name:$version"
+}
+
+build
